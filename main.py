1d1cd577e991922887e1df4776440861570f3493
JungyuOO <rlawnsrb731@google.com>
2025-08-08 11:53:53 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import time
+import requests
+import discord
+from discord.ext import commands
+
+def get_latest_menu_image():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=options)
+
+    url = 'https://pf.kakao.com/_xfWxfCxj/posts'
+    driver.get(url)
+    time.sleep(2)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    img_url = None
+    for img in soup.select('img'):
+        src = img.get('src')
+        if src and 'kakaocdn.net' in src and (src.endswith('.jpg') or src.endswith('.png')):
+            img_url = src
+            break
+    driver.quit()
+    return img_url
+
+def download_image(img_url, filename='today_menu.jpg'):
+    img_data = requests.get(img_url).content
+    with open(filename, "wb") as f:
+        f.write(img_data)
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+async def 점심(ctx):
+    img_url = get_latest_menu_image()
+    if img_url:
+        download_image(img_url)
+        file = discord.File("today_menu.jpg")
+        await ctx.send(file=file)
+    else:
+        await ctx.send("오늘의 메뉴 이미지를 찾지 못했습니다.")
+
+bot.run('YOUR_DISCORD_BOT_TOKEN')
